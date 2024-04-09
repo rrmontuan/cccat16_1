890279856fe134400905cce725ccbd669a264247
Ricardo Montuan <ricardo.montuan@hotmail.com>
2024-04-09 14:46:24 -0300

    refactor: refactor api using signup use case

--- src/api.ts
+++ src/api.ts
@@ -1,69 +1,20 @@
-import crypto from "crypto";
-import express from "express";
-import pgp from "pg-promise";
-import { validate } from "./validateCpf";
+import express, { Request } from "express";
+import PostgresDBImpl from "./infrastructure/PostgresDBImpl";
+import { IDB } from "./infrastructure/IDB";
+import { AccountRepository } from "./modules/identity/application/repositories/AccountRepository";
+import { SignUp, SignUpDTO } from "./modules/identity/application/useCases/signUp";
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async function (req, res) {
-	let result;
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const id = crypto.randomUUID();
+app.post("/signup", async function (req: Request<{}, {}, SignUpDTO>, res) {
+  const dbConnection: IDB = PostgresDBImpl.getInstance();
+  const accountRepository = new AccountRepository(dbConnection);
+  const signUp = new SignUp(accountRepository);
+  const result = await signUp.execute(req.body)
 
-		const [acc] = await connection.query("select * from cccat15.account where email = $1", [req.body.email]);
-		if (!acc) {
+  if (result.isLeft()) return res.status(422).send(result.error.code + "");
 
-			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (req.body.email.match(/^(.+)@(.+)$/)) {
-
-					if (validate(req.body.cpf)) {
-						if (req.body.isDriver) {
-							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
-
-			} else {
-				// invalid name
-				result = -3;
-			}
-
-		} else {
-			// already exists
-			result = -4;
-		}
-		if (typeof result === "number") {
-			res.status(422).send(result + "");
-		} else {
-			res.json(result);
-		}
-	} finally {
-		await connection.$pool.end();
-	}
+  return res.json(result.value)
 });
 
-app.listen(3000);
+export default app
